7f42d7e8f1f1448dd27240b9819b9bfef411b095
Navdeep0026 <navdeepkaur3190@conestogac.on.ca>
2024-06-13 18:19:57 -0400

    Update Program.cs

--- Gemhunters1/Gemhunters1/Program.cs
+++ Gemhunters1/Gemhunters1/Program.cs
@@ -122,3 +122,12 @@ public class Game
         }
     }
 }
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Game game = new Game();
+        game.Start();
+    }
+}
