ec098fa5d0b2ba545c29e043c6e39bb22bb8ae61
Navdeep0026 <navdeepkaur3190@conestogac.on.ca>
2024-06-14 19:34:41 -0400

    Commit 3

--- Gemhunters1/Gemhunters1/Program.cs
+++ Gemhunters1/Gemhunters1/Program.cs
@@ -12,6 +12,11 @@ namespace GemHunters
             X = x;
             Y = y;
         }
+
+        public bool Equals(Position other)
+        {
+            return X == other.X && Y == other.Y;
+        }
     }
 
     public class Player
@@ -25,6 +30,17 @@ namespace GemHunters
             Name = name;
             Position = position;
             GemCount = 0;
+        }
+
+        public void Move(char direction)
+        {
+            switch (direction)
+            {
+                case 'U': Position.Y = Math.Max(0, Position.Y - 1); break;
+                case 'D': Position.Y = Math.Min(5, Position.Y + 1); break;
+                case 'L': Position.X = Math.Max(0, Position.X - 1); break;
+                case 'R': Position.X = Math.Min(5, Position.X + 1); break;
+            }
         }
     }
 
@@ -80,13 +96,42 @@ namespace GemHunters
             board[x, y] = item;
         }
 
-        public void Display()
+        public bool IsGem(Position position)
+        {
+            return board[position.X, position.Y] == 'G';
+        }
+
+        public bool IsObstacle(Position position)
+        {
+            return board[position.X, position.Y] == 'O';
+        }
+
+        public void RemoveGem(Position position)
+        {
+            if (board[position.X, position.Y] == 'G')
+            {
+                board[position.X, position.Y] = '-';
+            }
+        }
+
+        public void Display(Player p1, Player p2)
         {
             for (int i = 0; i < Size; i++)
             {
                 for (int j = 0; j < Size; j++)
                 {
-                    Console.Write(board[i, j] + " ");
+                    if (p1.Position.X == i && p1.Position.Y == j)
+                    {
+                        Console.Write("P1 ");
+                    }
+                    else if (p2.Position.X == i && p2.Position.Y == j)
+                    {
+                        Console.Write("P2 ");
+                    }
+                    else
+                    {
+                        Console.Write(board[i, j] + " ");
+                    }
                 }
                 Console.WriteLine();
             }
@@ -98,8 +143,51 @@ namespace GemHunters
         static void Main(string[] args)
         {
             Console.WriteLine("Gem Hunters Game!");
+
+            Player player1 = new Player("P1", new Position(0, 0));
+            Player player2 = new Player("P2", new Position(5, 5));
             Board board = new Board();
-            board.Display();
+
+            for (int i = 0; i < 30; i++)
+            {
+                board.Display(player1, player2);
+                Player currentPlayer = i % 2 == 0 ? player1 : player2;
+                Console.WriteLine($"{currentPlayer.Name}'s turn. Enter move (U, D, L, R):");
+                char move = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                Position oldPosition = new Position(currentPlayer.Position.X, currentPlayer.Position.Y);
+                currentPlayer.Move(move);
+
+                if (board.IsObstacle(currentPlayer.Position))
+                {
+                    currentPlayer.Position = oldPosition;
+                    Console.WriteLine("Move blocked by an obstacle!");
+                }
+                else if (board.IsGem(currentPlayer.Position))
+                {
+                    currentPlayer.GemCount++;
+                    board.RemoveGem(currentPlayer.Position);
+                }
+
+                Console.Clear();
+            }
+
+            Console.WriteLine("Game Over!");
+            Console.WriteLine($"Player 1 Gems: {player1.GemCount}");
+            Console.WriteLine($"Player 2 Gems: {player2.GemCount}");
+            if (player1.GemCount > player2.GemCount)
+            {
+                Console.WriteLine("Player 1 Wins!");
+            }
+            else if (player2.GemCount > player1.GemCount)
+            {
+                Console.WriteLine("Player 2 Wins!");
+            }
+            else
+            {
+                Console.WriteLine("It's a tie!");
+            }
         }
     }
 }
